f82cdfe1f01f788497f5170c47d291eb3b7b944b
yrochat <yannick.rochat@gmail.com>
2015-03-27 17:05:36 +0100

    Ajout du visuel
    fonction plot pour le visuel
    fonction pdf() pour l’output

--- Exemple/exemple-assomoir.R
+++ Exemple/exemple-assomoir.R
@@ -1,6 +1,9 @@
 rm(list=ls())
 
-library(igraph)
+# Ici on charge le package igraph qui sert à faire de l'analyse de réseaux
+# si ça ne fonctionne pas, il faut d'abord le télécharger. Pour ça :
+# install.packages("igraph")
+library(igraph) 
 
 setwd("~/Documents/Belfort/Exemple")
 
@@ -48,6 +51,48 @@ g <- bipartite.projection(g0)$proj1
 g <- g - E(g)[E(g)$weight < 3]
 
 
+############
+### PLOT ###
+############
+
+# Facilitons-nous la vie dans l'urgence de la conférence : 
+# nous n'allons dessiner que la composante géante
+g <- induced.subgraph(g, vids = which(clusters(g)$membership == which.max(clusters(g)$csize)))
+
+# Ici on crée le fichier où va arriver le visuel
+# On lui donne les dimensions qu'on veut
+# Cela crée une sorte de tunnel qui enregistrera tous nos ajouts
+# Et qu'on ferme à la fin avec la fonction dev.off()
+pdf("exemple.pdf", height = 15, width = 15)
+
+# À ce stade et pour mémoire, le tunnel est ouvert
+# Ce qu'on fait ici est de dire qu'il n'y a aucune marge 
+# et que le graphe doit prendre toute la place
+par(mar=c(0,0,0,0))
+
+# Là on dessine le graphe, avec plein d'ajustements…
+# Pour plus de renseignements, taper dans la console la commande suivant :
+# ?igraph.plotting
+plot(g,
+		vertex.label = paste(substr(V(g)$name, 1, 6), ".", sep=""),
+		vertex.size = log2(degree(g))-1,
+		vertex.color = "black",
+		vertex.label.color = "black",
+		vertex.label.dist = (log2(degree(g))-.2)/20,
+		vertex.label.family = "sans", 
+		edge.width = log2(E(g)$weight) / 10,
+		edge.color = "darkgrey",
+		layout = layout.fruchterman.reingold(g, repulserad = vcount(g)^3.5)
+	)
+
+# Et on ferme le tunnel
+dev.off()
+
+# Le résultat est assez illisible, et devra être amélioré au cas par cas
+# par exemple avec tkplot pour choisir un meilleur emplacement pour les noeuds
+# ou alors en changeant tailles, couleurs, etc.
+
+# À étudier : les différentes possibilités d'export en D3…
 
 
 
