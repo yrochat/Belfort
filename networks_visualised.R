54649da5afe79340701ad37b296b39a0df1e5d8c
Yannick Rochat <yannick.rochat@gmail.com>
2017-07-11 01:06:05 +0200

    Ajouté la visualisation de la distribution des degrés
    Pas forcément la représentation la plus judicieuse à vue de nez, mais attendons de voir sur plusieurs réseaux.
    Là il faut que je fasse du facettage ! (ou du mfrow !?)

--- networks_visualised.R
+++ networks_visualised.R
@@ -4,6 +4,7 @@ library(igraph)
 library(stringr)
 library(ggraph)
 library(readr)
+library(tidygraph)
 
 set_graph_style(plot_margin = margin(1,1,1,1))
 
@@ -18,11 +19,19 @@ list_of_adjacency_sources <- list_of_sources[str_detect(list_of_sources, "adj.cs
 
 g <- lapply(list_of_adjacency_sources, create_graph)
 
+g[[1]] <- as_tbl_graph(g[[1]])
+
+V(g[[1]])$degree <- degree(g[[1]])
+
 ggraph(g[[1]]) + 
   geom_node_point() + 
   geom_edge_link(aes(width = weight)) + 
   scale_edge_width_continuous(range = c(.1,2)) +
   geom_node_label(aes(label = name), size = 2, repel = TRUE, label.size = .1)
+
+ggplot(as_tibble(g[[1]]), aes(degree)) + 
+  stat_ecdf() +
+  theme_bw()
 
   
 ## TODO
